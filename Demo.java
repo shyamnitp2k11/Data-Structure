ccf70340b3ac23a3b4a47f76b378f1fe76482c71
Shyam Kishor <shyamkishor@Shyams-MacBook-Air.local>
2018-09-01 22:59:41 +0530

    First Commit

---
+++ Demo.java
@@ -0,0 +1,25 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		String str="java";
+		
+		
+String str1="java";
+System.out.print(""+str==str1);
+	}
+
+}
+
+abstract class aba
+{
+	abstract void m() ;
+	
+}
+class b extends aba
+{
+	void m()
+	{
+		//sklndkln
+	}
+}

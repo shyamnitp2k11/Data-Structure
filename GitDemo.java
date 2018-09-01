14ea16a08f90928024da3e624beff79eac331b59
shyamnitp2k11 <shyamnitp2k11@gmail.com>
2018-09-01 23:13:47 +0530

    Update GitDemo.java

--- GitDemo.java
+++ GitDemo.java
@@ -3,7 +3,7 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.print("Demo of git ");
+            System.out.print("Demo of git pull from server....");
 	}
 
 }
